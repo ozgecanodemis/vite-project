7be42a9ec1c6dc74b9347a29760b8d26d4f91522
ozgecanodemis <odemisozgecan@gmail.com>
2025-04-09 23:33:05 +0300

    ss

--- my-vite-project/src/components/Content.jsx
+++ my-vite-project/src/components/Content.jsx
@@ -16,23 +16,24 @@ function Content() {
 
     return (
         <div className="bg-white dark:bg-gray-900 transition-colors duration-300">
-            <div className="m-auto max-w-[1140px] mx-auto">
-                {/* Dark Mode Toggle and Language Switcher */}
+
+            <div className="m-auto max-w-[1140px] mx-auto ">
+                {/* Dark Mode Toggle ve Language Switcher */}
                 <div className="mt-[20px] flex justify-end mb-6 dark:text-[#B7AAFF]">
                     <DarkModeBtn />
                     <span className="mx-2 text-gray-800 dark:text-[#B7AAFF]">|</span>
                     <Languagetxt />
                 </div>
 
-                {/* Avatar and Info Section */}
-                <div className="flex flex-col md:flex-row items-center justify-between">
+                {/* Avatar ve Info Section */}
+                <div className="flex items-center justify-between">
                     <div className="flex items-center">
                         {/* Avatar Component */}
                         <Avatar />
                     </div>
 
-                    {/* Skills, Projects, and Hire Me Section */}
-                    <div className="ml-6 text-left flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-12 mt-4 md:mt-0">
+                    {/* Skills, Projects, ve Hire Me Section */}
+                    <div className="ml-6 text-left flex space-x-12">
                         <p
                             className="text-[#6B7280] dark:text-[#6B7280] mb-2 translate-y-[6px] cursor-pointer"
                             onClick={() => scrollToSection('skills-section')}
